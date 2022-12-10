39a8a5efc31ece47e22885081685c3a68f03e0b2
BreffJaun <braun_jeff@web.de>
2022-12-10 12:37:28 +0100

    MongoDB and Port Changes

--- server.js
+++ server.js
@@ -16,10 +16,11 @@ import { errorHandler } from './middleware/errorhandler.js';
 // C O N N E C T   W I T H   M O N G O O S E  D B
 mongoose.set("strictQuery", false); // to prevent an erroneous error message
 mongoose.set("strictQuery", false);
-const DB_CONNECTION_STRING =  process.env.MONGO_DB_CONNECTION_STRING
+const DB_CONNECTION_STRING =  process.env.MONGO_DB_CONNECTION_STRING || "mongodb://localhost:27017"
+const PORT = process.env.PORT || 4000
 mongoose.connect(DB_CONNECTION_STRING)
-.then(() => console.log('Connect with MongoDB: SUCCESS '))
-.catch((err) => console.log('Connect with MongoDB: FAILED ', err))
+.then(() => console.log('Connect with MongoDB: SUCCESS ✅'))
+.catch((err) => console.log('Connect with MongoDB: FAILED ⛔', err))
 // for errors which comes after the successfully connection
 mongoose.connection.on('error', console.log);
 
